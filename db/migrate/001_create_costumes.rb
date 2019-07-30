f76e71f03e0689563b884735fc0f95ad31851b31
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-30 02:42:16 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.url :image_url
+      t.string :image_url
       t.string :size
       t.timestamps
     end
