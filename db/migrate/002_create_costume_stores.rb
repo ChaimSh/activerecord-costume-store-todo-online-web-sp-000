3ea0e94799b2351451c9415ecbd3907162ddab5a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-30 02:56:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.time :opening_time
-      t.time :closing_time
+      t.time.now :closing_time
     end
   end
 
